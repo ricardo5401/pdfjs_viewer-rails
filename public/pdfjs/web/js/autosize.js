7d7b29a1c785ef22174de42514dcd3c6ef060112
Ricardo <ricardo.izasiga@gmail.com>
2016-01-12 11:45:32 -0500

    try_autosize

--- public/pdfjs/web/js/autosize.js
+++ public/pdfjs/web/js/autosize.js
@@ -6,5 +6,6 @@ $(document).ready(function(){
 });
 function setSelectBoxByIndex() {
   document.getElementById("scaleSelect").options[0].selected = 'selected';
+  alert(document.getElementById("scaleSelect").options[0].text);
   //autoZoom
 }
